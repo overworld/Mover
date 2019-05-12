27baa32129ca4dc654aa975216017386aef3e774
ethan <lordor-war@hotmail.fr>
2019-05-12 20:46:01 +0200

    update 1

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Search from './Components/Search'
+import LandScreen from './Components/LandScreen'
+
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+        <LandScreen/>
+        //<Search/>
+    );
+  }
+}
+
+
