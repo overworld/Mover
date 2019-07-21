df6b03aba4961d6bbbcc8b0fed95b8f02cfab65d
ethan <lordor-war@hotmail.fr>
2019-07-21 17:47:16 +0200

    Récuperation et affichage des events

--- Components/NavigateScreen.js
+++ Components/NavigateScreen.js
@@ -17,12 +17,38 @@ import {
     Right
 } from 'native-base';
 
+export const get = () => fetch('http://mover.florianthe.fr/api/event/getall')
+    .then((response) => response.json())
+    .then((responseJson) => {
+        return responseJson;
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+
 
 class NavigateScreen extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { loading: true };
+        this.state = {
+            'events': [],
+
+            loading: true,
+
+        };
+
+
     }
+
+    getEvents() {
+        return fetch('http://mover.florianthe.fr/api/event/getall')
+            .then((response) => response.json())
+            .then((response) => this.setState({'events': response}))
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
 
     async componentWillMount() {
         await Expo.Font.loadAsync({
@@ -30,293 +56,66 @@ class NavigateScreen extends React.Component {
             Roboto_medium: require("../node_modules/native-base/Fonts/Roboto_medium.ttf"),
             Ionicons: require("../node_modules/@expo/vector-icons/src/vendor/react-native-vector-icons/Fonts/Ionicons.ttf"),
         });
-        this.setState({ loading: false });
+        this.getEvents();
+        this.setState({loading: false});
     }
 
 
     render() {
 
-        const loading = <Expo.AppLoading />;
+        const loading = <Expo.AppLoading/>;
+
 
         const showView = <Container>
             <Header/>
             <Content>
-                <Card>
-                    <CardItem>
-                        <Left>
-                            <Thumbnail
-                                source={require("../assets/logo.png")}/>
-                            <Body>
-                            <Text>NativeBase</Text>
-                            <Text note>GeekyAnts</Text>
-                            </Body>
-                        </Left>
-                    </CardItem>
-                    <CardItem cardBody>
-                        <Image
-                            source={require("../assets/BsoN209EVK0EiHQDCs2Cfg.XmvlPQeNpSlK5rZlvkEPbg.adc620.jpg")}
-                            style={{height: 200, width: null, flex: 1}}/>
-                    </CardItem>
-                    <CardItem>
-                        <Left>
-                            <Button transparent>
-                                <Icon active name="thumbs-up"/>
-                                <Text>12 Likes</Text>
-                            </Button>
-                        </Left>
-                        <Body>
-                        <Button transparent>
-                            <Icon active name="chatbubbles"/>
-                            <Text>4 Comments</Text>
-                        </Button>
-                        </Body>
-                        <Right>
-                            <Text>11h ago</Text>
-                        </Right>
-                    </CardItem>
-
-                    <CardItem>
-                        <Left>
-                            <Thumbnail
-                                source={require("../assets/logo.png")}/>
-                            <Body>
-                            <Text>NativeBase</Text>
-                            <Text note>GeekyAnts</Text>
-                            </Body>
-                        </Left>
-                    </CardItem>
-                    <CardItem cardBody>
-                        <Image
-                            source={require("../assets/BsoN209EVK0EiHQDCs2Cfg.XmvlPQeNpSlK5rZlvkEPbg.adc620.jpg")}
-                            style={{height: 200, width: null, flex: 1}}/>
-                    </CardItem>
-                    <CardItem>
-                        <Left>
-                            <Button transparent>
-                                <Icon active name="thumbs-up"/>
-                                <Text>12 Likes</Text>
-                            </Button>
-                        </Left>
-                        <Body>
-                        <Button transparent>
-                            <Icon active name="chatbubbles"/>
-                            <Text>4 Comments</Text>
-                        </Button>
-                        </Body>
-                        <Right>
-                            <Text>11h ago</Text>
-                        </Right>
-                    </CardItem>
-
-                    <CardItem>
-                        <Left>
-                            <Thumbnail
-                                source={require("../assets/logo.png")}/>
-                            <Body>
-                            <Text>NativeBase</Text>
-                            <Text note>GeekyAnts</Text>
-                            </Body>
-                        </Left>
-                    </CardItem>
-                    <CardItem cardBody>
-                        <Image
-                            source={require("../assets/BsoN209EVK0EiHQDCs2Cfg.XmvlPQeNpSlK5rZlvkEPbg.adc620.jpg")}
-                            style={{height: 200, width: null, flex: 1}}/>
-                    </CardItem>
-                    <CardItem>
-                        <Left>
-                            <Button transparent>
-                                <Icon active name="thumbs-up"/>
-                                <Text>12 Likes</Text>
-                            </Button>
-                        </Left>
-                        <Body>
-                        <Button transparent>
-                            <Icon active name="chatbubbles"/>
-                            <Text>4 Comments</Text>
-                        </Button>
-                        </Body>
-                        <Right>
-                            <Text>11h ago</Text>
-                        </Right>
-                    </CardItem>
 
 
-                    <CardItem>
-                        <Left>
-                            <Thumbnail
-                                source={require("../assets/logo.png")}/>
-                            <Body>
-                            <Text>NativeBase</Text>
-                            <Text note>GeekyAnts</Text>
-                            </Body>
-                        </Left>
-                    </CardItem>
-                    <CardItem cardBody>
-                        <Image
-                            source={require("../assets/BsoN209EVK0EiHQDCs2Cfg.XmvlPQeNpSlK5rZlvkEPbg.adc620.jpg")}
-                            style={{height: 200, width: null, flex: 1}}/>
-                    </CardItem>
-                    <CardItem>
-                        <Left>
-                            <Button transparent>
-                                <Icon active name="thumbs-up"/>
-                                <Text>12 Likes</Text>
-                            </Button>
-                        </Left>
-                        <Body>
-                        <Button transparent>
-                            <Icon active name="chatbubbles"/>
-                            <Text>4 Comments</Text>
-                        </Button>
-                        </Body>
-                        <Right>
-                            <Text>11h ago</Text>
-                        </Right>
-                    </CardItem>
+                                {this.state.events.map(function (event, index,) {
+                                    return (
+                                        <Card>
+
+                                        <CardItem>
+                                            <Left>
+                                                <Thumbnail
+                                                    source={require("../assets/logo.png")}/>
+                                                <Body>
+                                                    <Text>{event.name}</Text>
+                                                    <Text note> {event.introduction} </Text>
+                                                </Body>
+                                            </Left>
+                                        </CardItem>
+
+                                            <CardItem>
+                                                <Left>
+                                                    <Button transparent>
+                                                        <Icon active name="thumbs-up"/>
+                                                        <Text>12 Likes</Text>
+                                                    </Button>
+                                                </Left>
+                                                <Body>
+                                                    <Button transparent>
+                                                        <Icon active name="chatbubbles"/>
+                                                        <Text>4 Comments</Text>
+                                                    </Button>
+                                                </Body>
+                                                <Right>
+                                                    <Text>{event.dateBegin}</Text>
+                                                </Right>
+                                            </CardItem>
 
 
-                    <CardItem>
-                        <Left>
-                            <Thumbnail
-                                source={require("../assets/logo.png")}/>
-                            <Body>
-                            <Text>NativeBase</Text>
-                            <Text note>GeekyAnts</Text>
-                            </Body>
-                        </Left>
-                    </CardItem>
-                    <CardItem cardBody>
-                        <Image
-                            source={require("../assets/BsoN209EVK0EiHQDCs2Cfg.XmvlPQeNpSlK5rZlvkEPbg.adc620.jpg")}
-                            style={{height: 200, width: null, flex: 1}}/>
-                    </CardItem>
-                    <CardItem>
-                        <Left>
-                            <Button transparent>
-                                <Icon active name="thumbs-up"/>
-                                <Text>12 Likes</Text>
-                            </Button>
-                        </Left>
-                        <Body>
-                        <Button transparent>
-                            <Icon active name="chatbubbles"/>
-                            <Text>4 Comments</Text>
-                        </Button>
-                        </Body>
-                        <Right>
-                            <Text>11h ago</Text>
-                        </Right>
-                    </CardItem>
+                                        </Card>
+                                    )
+                                })}
 
-
-                    <CardItem>
-                        <Left>
-                            <Thumbnail
-                                source={require("../assets/logo.png")}/>
-                            <Body>
-                            <Text>NativeBase</Text>
-                            <Text note>GeekyAnts</Text>
-                            </Body>
-                        </Left>
-                    </CardItem>
-                    <CardItem cardBody>
-                        <Image
-                            source={require("../assets/BsoN209EVK0EiHQDCs2Cfg.XmvlPQeNpSlK5rZlvkEPbg.adc620.jpg")}
-                            style={{height: 200, width: null, flex: 1}}/>
-                    </CardItem>
-                    <CardItem>
-                        <Left>
-                            <Button transparent>
-                                <Icon active name="thumbs-up"/>
-                                <Text>12 Likes</Text>
-                            </Button>
-                        </Left>
-                        <Body>
-                        <Button transparent>
-                            <Icon active name="chatbubbles"/>
-                            <Text>4 Comments</Text>
-                        </Button>
-                        </Body>
-                        <Right>
-                            <Text>11h ago</Text>
-                        </Right>
-                    </CardItem>
-
-
-                    <CardItem>
-                        <Left>
-                            <Thumbnail
-                                source={require("../assets/logo.png")}/>
-                            <Body>
-                            <Text>NativeBase</Text>
-                            <Text note>GeekyAnts</Text>
-                            </Body>
-                        </Left>
-                    </CardItem>
-                    <CardItem cardBody>
-                        <Image
-                            source={require("../assets/BsoN209EVK0EiHQDCs2Cfg.XmvlPQeNpSlK5rZlvkEPbg.adc620.jpg")}
-                            style={{height: 200, width: null, flex: 1}}/>
-                    </CardItem>
-                    <CardItem>
-                        <Left>
-                            <Button transparent>
-                                <Icon active name="thumbs-up"/>
-                                <Text>12 Likes</Text>
-                            </Button>
-                        </Left>
-                        <Body>
-                        <Button transparent>
-                            <Icon active name="chatbubbles"/>
-                            <Text>4 Comments</Text>
-                        </Button>
-                        </Body>
-                        <Right>
-                            <Text>11h ago</Text>
-                        </Right>
-                    </CardItem>
-
-
-                    <CardItem>
-                        <Left>
-                            <Thumbnail
-                                source={require("../assets/logo.png")}/>
-                            <Body>
-                            <Text>NativeBase</Text>
-                            <Text note>GeekyAnts</Text>
-                            </Body>
-                        </Left>
-                    </CardItem>
-                    <CardItem cardBody>
-                        <Image
-                            source={require("../assets/BsoN209EVK0EiHQDCs2Cfg.XmvlPQeNpSlK5rZlvkEPbg.adc620.jpg")}
-                            style={{height: 200, width: null, flex: 1}}/>
-                    </CardItem>
-                    <CardItem>
-                        <Left>
-                            <Button transparent>
-                                <Icon active name="thumbs-up"/>
-                                <Text>12 Likes</Text>
-                            </Button>
-                        </Left>
-                        <Body>
-                        <Button transparent>
-                            <Icon active name="chatbubbles"/>
-                            <Text>4 Comments</Text>
-                        </Button>
-                        </Body>
-                        <Right>
-                            <Text>11h ago</Text>
-                        </Right>
-                    </CardItem>
-                </Card>
             </Content>
         </Container>;
 
         return (
             <View style={styles.MainContainer}>
                 {this.state.loading === false ? showView : loading}
+
             </View>
         )
     }
